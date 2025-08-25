41df89d967f16d363782c14a0b3bfb2cf7776c56
Eunille Jan Apo <beerusgaming24@gmail.com>
2025-08-25 20:16:18 +0800

    Added Images and export it in the assetss/index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
 import Cloud from "../assets/img/Cloud.png";
 import Network from "../assets/img/Networks.png";
 import Cybersecurity from "../assets/img/Cybersecurity.png";
+import HackTheBox from "../assets/img/HackTheBox.jpg"
+import PreSecurity from "../assets/img/PreSecurity"
 
-
-export { Cloud, Network, Cybersecurity };
+export { Cloud, Network, Cybersecurity, HackTheBox, PreSecurity };
